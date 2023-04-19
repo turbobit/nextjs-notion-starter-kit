5e54a9f45b9821fdfce35e7019087bf7c91e68e7
turbobit <turbobit@closetoya.com>
2023-04-19 11:01:27 +0900

    init - 대상 https://www.notion.so/test-c17c2072bfdb46a8899aadc0e646a5fe?pvs=2 으로 변경

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3a3f7bbc2fd041d7bd724d4a06e527c6',
+  rootNotionPageId: 'c17c2072bfdb46a8899aadc0e646a5fe',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  github: 'Gixist/this.is.the.ai',
+  //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
